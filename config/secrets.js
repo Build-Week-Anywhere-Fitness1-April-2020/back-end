ef2f71d40467dc2303b09069ab0b9bf29e12647a
Mina Ramzey <52507627+Minaramzey@users.noreply.github.com>
2020-04-26 13:01:40 -0400

    error w/ merge

--- config/secrets.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    jwtSecret : process.env.JWT_SECRET || 'Secret secret secret'
-}
--- data/dbConfig.js
+++
@@ -1,6 +0,0 @@
-const knex = require('knex');
-const knexFile = require('../knexfile');
-
-const enviornment = process.env.DB_ENV || 'development';
-
-module.exports = knex(knexFile[enviornment]);
