f12383a65b6699751a1a1d63649ab197c5cc0f08
Josiah Roa <josiahroa18@gmail.com>
2020-04-29 09:55:22 -0600

    Seeded tables

--- data/seeds/03-accountRoles.js
+++ data/seeds/03-accountRoles.js
@@ -7,6 +7,7 @@ exports.seed = function(knex) {
     .then(function() {
       return knex("accountRoles").insert([
           {accountId: 1, roleId: 1},
+          {accountId: 1, roleId: 2},
           {accountId: 2, roleId: 1},
           {accountId: 3, roleId: 2},
           {accountId: 4, roleId: 2}
